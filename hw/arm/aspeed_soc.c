43b11b57c478125b9a1fe1a271d077cfffde64d4
Fred Shao <duanzhang512@users.noreply.github.com>
2020-02-08 12:18:05 +0800

    Add uart1 support for AST2400 and AST2500
    Add uart1 support for BMC AST2400 and AST2500.

--- hw/arm/aspeed_soc.c
+++ hw/arm/aspeed_soc.c
@@ -305,6 +305,9 @@ static void aspeed_soc_realize(DeviceState *dev, Error **errp)
 
     /* UART - attach an 8250 to the IO space as our UART5 */
     if (serial_hd(0)) {
+        qemu_irq uart1 = aspeed_soc_get_irq(s, ASPEED_UART1);
+        serial_mm_init(get_system_memory(), sc->memmap[ASPEED_UART1], 2,
+                       uart1, 38400, serial_hd(0), DEVICE_LITTLE_ENDIAN);        
         qemu_irq uart5 = aspeed_soc_get_irq(s, ASPEED_UART5);
         serial_mm_init(get_system_memory(), sc->memmap[ASPEED_UART5], 2,
                        uart5, 38400, serial_hd(0), DEVICE_LITTLE_ENDIAN);
